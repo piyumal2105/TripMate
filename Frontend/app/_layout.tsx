b136a907bdc4a51c59695913a132a45959f433aa
Piyumal Madhuwantha <121705632+piyumal2105@users.noreply.github.com>
2025-03-14 20:15:29 +0530

    commit changes

--- Frontend/app/_layout.tsx
+++ Frontend/app/_layout.tsx
@@ -1,12 +1,3 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index" />
-//     </Stack>
-//   );
-// }
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
