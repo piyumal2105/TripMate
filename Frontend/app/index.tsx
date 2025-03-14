96bd2709d4a9a12b64ade847cca9aa89588bddf9
Piyumal Madhuwantha <121705632+piyumal2105@users.noreply.github.com>
2025-03-14 16:02:32 +0530

    commit changes

---
+++ Frontend/app/index.tsx
@@ -0,0 +1,27 @@
+import { Text, View } from "react-native";
+import Login from "./../components/Login";
+import {auth} from './../configs/FirebaseConfig'
+import { Redirect } from "expo-router";
+import { useEffect } from "react";
+import { useRouter } from 'expo-router';
+
+export default function Index() {
+
+  const user=auth.currentUser;
+  
+  return (
+    <View
+      style={{
+        flex: 1,
+        
+      }}
+    >
+
+      {user?
+    <Redirect href={'/mytrip'}/>:
+     <Login/>
+      }
+
+    </View>
+  );
+}
